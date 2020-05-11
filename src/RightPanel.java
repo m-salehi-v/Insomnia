675d901fc454499ab5845be01ae4b3d567bfc93a
9831037 <9831037@ceit.local>
2020-05-12 00:08:52 +0430

    right panel's top panel is added

--- src/RightPanel.java
+++ src/RightPanel.java
@@ -1,7 +1,64 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class RightPanel extends JPanel {
+    private JPanel topPanel;
+
     public RightPanel() {
         super();
+        setLayout(new BorderLayout());
+
+        topPanelInit();
+        add(topPanel, BorderLayout.NORTH);
+    }
+
+
+    private void topPanelInit() {
+        topPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 9));
+        topPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY));
+
+        topPanel.add(createStatusLabel(0, "Error"));
+        topPanel.add(createTimeLabel(0));
+        topPanel.add(createSizeLabel(0));
+    }
+
+    private JLabel createStatusLabel(int statusCode, String statusMessage) {
+        JLabel statusLabel = new JLabel();
+        if (statusMessage.equals("Error")) {
+            statusLabel.setText("Error");
+            statusLabel.setBackground(new Color(199, 40, 40));
+        } else {
+            statusLabel.setText(statusCode + " " + statusMessage);
+            statusLabel.setBackground(new Color(55, 171, 46));
+        }
+        statusLabel.setForeground(Color.WHITE);
+        statusLabel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        statusLabel.setFont(new Font(null, Font.BOLD, 12));
+        statusLabel.setOpaque(true);
+        return statusLabel;
+    }
+
+    private JLabel createTimeLabel(double time) {
+        JLabel statusLabel = new JLabel();
+        statusLabel.setText("TIME " + time + " s");
+        statusLabel.setBackground(new Color(188, 191, 187));
+        statusLabel.setForeground(Color.darkGray);
+        statusLabel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        statusLabel.setFont(new Font(null, Font.PLAIN, 12));
+        statusLabel.setOpaque(true);
+        return statusLabel;
+    }
+
+    private JLabel createSizeLabel(double size){
+
+        JLabel statusLabel = new JLabel();
+        statusLabel.setText("SIZE " + size + " KB");
+        statusLabel.setBackground(new Color(188, 191, 187));
+        statusLabel.setForeground(Color.darkGray);
+        statusLabel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        statusLabel.setFont(new Font(null, Font.PLAIN, 12));
+        statusLabel.setOpaque(true);
+        return statusLabel;
     }
 }
